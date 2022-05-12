c459cbc63c2ba92df70c7deee9c2dcf8369e0f1b
Teodor G <grigor.teodor@gmail.com>
2022-05-12 22:43:31 +0300

    website deployed

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': 'rgb(20 22 26 / 0.9)'
+      },
+      transitionDuration: {
+        '400': '400ms',
+      },
+      margin: {
+        '110': '30rem',
+        '128': '40rem',
+        '162': '62rem',
+        '174': '68rem',
+        '180': '75rem',
+      }
+    },
+  },
+  plugins: [],
+}
